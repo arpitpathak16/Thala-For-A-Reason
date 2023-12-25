af2fbee75ee50cec193d4d22b74de543e5aac2d5
arpitpathak16 <110929773+arpitpathak16@users.noreply.github.com>
2023-12-26 02:37:11 +0530

    Add files via upload

--- thala.js
+++ thala.js
@@ -56,10 +56,45 @@ function handleSearch() {
       video.play();
     }
   } else {
-    var count = searchText.length;
-    if (searchText.length === 7 || searchText === "7") {
-      playerDiv.innerHTML =
-        "<h2>'Total is 7. Clearly,Thala For A Reason 💪'</h2>";
+    if (searchText === " " || searchText === "") {
+      playerDiv.innerHTML = "<h2>'Toot gya me to 😔'</h2>";
+      playerDiv.innerHTML +=
+        "<video id='video'><source src='./assets/toot gya.mp4' /></video>";
+      var video = document.getElementById("video");
+      video.currentTime = 0;
+      video.endTime = 30;
+      video.play();
+    } else if (searchText === "Anushka" || searchText === "anushka") {
+      playerDiv.innerHTML = "<h2>'This time, Chiku For A Reason 😎'</h2>";
+      playerDiv.innerHTML +=
+        "<video id='video'><source src='./assets/Virat_dance.mp4' /></video>";
+      var video = document.getElementById("video");
+      video.play();
+    } else if (
+      searchText.length === 7 ||
+      searchText === "7" ||
+      searchText === "seven" ||
+      searchText === "SEVEN" ||
+      searchText === "Seven" ||
+      searchText === "MSD" ||
+      searchText === "Dhoni" ||
+      searchText === "Thala" ||
+      searchText === "Mahi" ||
+      searchText === "MAHI" ||
+      searchText === "thala" ||
+      searchText === "dhoni" ||
+      searchText === "msd" ||
+      searchText === "mahi" ||
+      searchText === "DHONI" ||
+      searchText === "jiva" ||
+      searchText === "JIVA" ||
+      searchText === "Jiva" ||
+      searchText === "Sakshi" ||
+      searchText === "sakshi" ||
+      searchText === "Ranchi" ||
+      searchText === "ranchi"
+    ) {
+      playerDiv.innerHTML = "<h2>'Clearly,Thala For A Reason 💪'</h2>";
       playerDiv.innerHTML +=
         "<video id='video'><source src='./assets/Dhoni Meme _ Thala for A reason _ Dhoni.mp4' /></video>";
       var video = document.getElementById("video");
@@ -69,7 +104,22 @@ function handleSearch() {
     } else if (
       searchText.length === 9 ||
       searchText === "9" ||
-      searchText === "18"
+      searchText === "18" ||
+      searchText === "Virat" ||
+      searchText === "Chiku" ||
+      searchText === "Anushka" ||
+      searchText === "Virushka" ||
+      searchText === "virat" ||
+      searchText === "chiku" ||
+      searchText === "anushka" ||
+      searchText === "virushka" ||
+      searchText === "Kohli" ||
+      searchText === "kohli" ||
+      searchText === "Vamika" ||
+      searchText === "vamika" ||
+      searchText === "chole" ||
+      searchText === "Delhi" ||
+      searchText === "delhi"
     ) {
       playerDiv.innerHTML = "<h2>'This time, Chiku For A Reason 😎'</h2>";
       playerDiv.innerHTML +=
@@ -77,9 +127,10 @@ function handleSearch() {
       var video = document.getElementById("video");
       video.play();
     } else {
-      playerDiv.innerHTML = "<h2>'Toot gya me to 😔'</h2>";
+      playerDiv.innerHTML =
+        "<h2>'Thala is not for a reason krke aukat dikhadi 😔'</h2>";
       playerDiv.innerHTML +=
-        "<video id='video'><source src='./assets/toot gya.mp4' /></video>";
+        "<video id='video'><source src='./assets/Aukat dikha di meme.mp4' /></video>";
       var video = document.getElementById("video");
       video.currentTime = 0;
       video.endTime = 30;
